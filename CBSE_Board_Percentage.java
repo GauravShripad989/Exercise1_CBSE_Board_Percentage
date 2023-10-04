90102fe1d5c7020bae82863a2fe40ea3517cce00
GauravShripad989 <gauravshripad127@gmail.com>
2023-10-05 05:14:57 +0530

    New Class For claculating percentage

---
+++ CBSE_Board_Percentage.java
@@ -0,0 +1,10 @@
+package Exercise1_CBSE_Board_Percentage;
+
+public class CBSE_Board_Percentage {
+    /* Write a program to  Calculate Percentage of a given student in
+    CBSE Board Exam.His Marks from 5 Subjects must be taken as input
+    from the keyboard */
+    public static void main(String[] args){
+
+    }
+}
